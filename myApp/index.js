7c74e79f48e3aa6c4bc3e1456f30b402f6e71a0c
lifeng <15823904221@163.com>
2023-02-22 18:12:35 +0800

    提交

--- myApp/index.js
+++ myApp/index.js
@@ -12,6 +12,18 @@ app.post('/getQuery',(req,res)=>{
   const query = req.query;//查询条件
   res.send(query)
 })
+//动态匹配参数
+app.post('/user/:id/:username',(req,res)=>{
+  console.log(req)
+  const params = req.params;
+  res.send(params.username+',你好,你的通行证为：'+params.id)
+})
+//静态资源先去最先声明的静态资源位置找
+//静态资源
+app.use(express.static('../build'));//静态资源的访问地址：http://localhost:3000/main.html
+//多个静态资源，参数一：创建虚拟路径前缀
+app.use('/static', express.static('../build'));//静态资源的访问地址：http://localhost:3000/static/main.html
+
 app.listen(port,()=>{
   console.log(`监听${port}`);
 })
