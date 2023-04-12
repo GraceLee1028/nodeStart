8d0442b79d3bed1ebd27870a09ad951863a46ee3
lifeng <15823904221@163.com>
2023-04-12 16:23:59 +0800

    调整

--- README.md
+++ README.md
@@ -13,3 +13,23 @@ node index.js
 ```
 
 ### 备注：当创建了新的文件、添加了新的中间件时注意重新启动服务
+
+# 解决前端跨域问题
+```
+# 安装
+npm install cors
+# 使用
+## 引用
+const cors = require("cors");
+//引入该全局中间件
+app.use(cors())
+```
+
+# cors的相关配置
+```
+cors({
+  //  origin: '*',
+    origin: ['https://www.section.io', 'https://www.google.com/'],
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+})
+```
--- expressDemo/index.js
+++ expressDemo/index.js
@@ -1,14 +1,38 @@
 const express = require("express")
 const app = express();
-const bodyParser = require("./middleware/custom-body-parse");
+//解决不同contentType的传参解析
+const bodyParser = require("body-parser")
+//解决跨域问题
+const cors = require("cors");
+app.use(cors({
+  credentials: true,
+  origin:"http://192.168.100.155:8002"
+  // origin:"*" // 如果credentials为true,则origin不能设置为"*"
+}));
+
 //全局中间件
 const loggerMiddleware = function(req,res,next){
   console.log(Date.now(),req.method,req.url)
   next();
 }
 app.use(loggerMiddleware)
+//解析表单数据的中间件【contentType：x-www-form-urlencoded】
+app.use(bodyParser.urlencoded({extended:false}));
+//解析【contentType：application/json】
+app.use(bodyParser.json());
+//post 表单解析
+app.post("/getData",function(req,res){
+  const body = req.body;  
+  console.log(req)
+  console.log(body)
+  res.send({
+    status:0,
+    msg:'请求成功',
+    data:body
+  })
+})
 //自定义中间件----解决接收的数据处理
-app.post("/sendFile",bodyParser,function(req,res){
+app.post("/sendFile",function(req,res){
   console.log(req.body)
   res.send(req.body)
 })
